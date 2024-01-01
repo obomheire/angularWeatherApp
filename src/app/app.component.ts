e138bbde4925e589ade2ada38e39f1300e3935cd
obomheire <obomheire@gmail.com>
2024-01-01 15:17:06 +0100

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,8 @@ export class AppComponent implements OnInit {
   weatherData?: WeatherData;
 
   ngOnInit(): void {
-    this.getWeatherData(this.cityName);
-    this.cityName = '';
+    // this.getWeatherData(this.cityName);
+    // this.cityName = '';
   }
 
   onSubmit() {
