5dd280ac881e42658e155779a54133fbe7721f92
obomheire <obomheire@gmail.com>
2024-01-01 13:44:00 +0100

    adding search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   weatherData?: WeatherData;
 
   ngOnInit(): void {
-    this.weatherService.getWeatherData('wellington').subscribe({
+    this.weatherService.getWeatherData('lagos').subscribe({
       next: (response) => {
         this.weatherData = response;
         // console.log(response);
