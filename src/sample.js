6e275e54f9372c7c6bec2ffbb5a9a2bb0e8071ef
obomheire <obomheire@gmail.com>
2024-01-21 08:21:35 +0100

    Added sample response

---
+++ src/sample.js
@@ -0,0 +1,28 @@
+// {
+//   coord: { lon: 174.7756, lat: -41.2866 },
+//   weather: [ { id: 800, main: 'Clear', description: 'clear sky', icon: '01n' } ],
+//   base: 'stations',
+//   main: {
+//     temp: 54.23,
+//     feels_like: 53.17,
+//     temp_min: 51.57,
+//     temp_max: 56.86,
+//     pressure: 1018,
+//     humidity: 81
+//   },
+//   visibility: 10000,
+//   wind: { speed: 8.05, deg: 10 },
+//   clouds: { all: 1 },
+//   dt: 1704109209,
+//   sys: {
+//     type: 2,
+//     id: 2007945,
+//     country: 'NZ',
+//     sunrise: 1704127888,
+//     sunset: 1704182220
+//   },
+//   timezone: 46800,
+//   id: 2179537,
+//   name: 'Wellington',
+//   cod: 200
+// }
